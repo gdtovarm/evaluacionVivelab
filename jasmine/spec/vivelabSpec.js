4bd7111a286a5bbbf40a9a086b9743990926d52e
German <gdtovarm@unal.edu.co>
2015-10-22 10:25:06 -0500

    added a test to cover the item class

--- jasmine/spec/vivelabSpec.js
+++ jasmine/spec/vivelabSpec.js
@@ -1,3 +1,8 @@
 describe("item", function(){
-	
+	it("should have set name, sellIn and quality values", function(){
+		var item = new Item("test Item",0,0);
+		expect(item.name).toEqual("test Item");
+		expect(item.sell_in).toEqual(0);
+		expect(item.quality).toEqual(0);
+	});
 });
