6293f68fefb3d6eda54ee46622c17f596c0fdc1b
German <gdtovarm@unal.edu.co>
2015-10-22 11:36:36 -0500

    Refactored duplicated code

--- jasmine/spec/vivelabSpec.js
+++ jasmine/spec/vivelabSpec.js
@@ -1,31 +1,35 @@
 describe("item", function(){
+	var item;
+
 	function createItem(sell_in,quality){
 		return new Item("test Item",sell_in,quality);
 	}
 
+	function createAndUpdateItem(sell_in,quality){
+		item = createItem(sell_in,quality);
+		item.update_quality();
+	}
+
 	it("should have set name, sellIn and quality values", function(){
-		var item = createItem(0,0);
+		item = createItem(0,0);
 		expect(item.name).toEqual("test Item");
 		expect(item.sell_in).toEqual(0);
 		expect(item.quality).toEqual(0);
 	});
 
 	it("should lose quality and reduce sellIn time when a day passed", function(){
-		var item = createItem(1,1);
-		item.update_quality();
+		createAndUpdateItem(1,1);
 		expect(item.sell_in).toEqual(0);
 		expect(item.quality).toEqual(0);
 	});
 
 	it("should lose quality at twice the speed when sellIn time is 0", function(){
-		var item = createItem(0,2);
-		item.update_quality();
+		createAndUpdateItem(0,2);
 		expect(item.quality).toEqual(0);
 	});
 
 	it("should never have less than 0 quality", function(){
-		var item = createItem(0,0);
-		item.update_quality();
+		createAndUpdateItem(0,0);
 		expect(item.quality).toEqual(0);
 	});
 });
