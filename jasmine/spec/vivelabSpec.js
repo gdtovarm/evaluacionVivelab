f661f715af4146e80a47812d057d382c9307cccf
German <gdtovarm@unal.edu.co>
2015-10-22 10:59:48 -0500

    small refactor for duplicated code

--- jasmine/spec/vivelabSpec.js
+++ jasmine/spec/vivelabSpec.js
@@ -1,13 +1,17 @@
 describe("item", function(){
+	function createItem(quality,sell_in){
+		return new Item("test Item",quality,sell_in);
+	}
+
 	it("should have set name, sellIn and quality values", function(){
-		var item = new Item("test Item",0,0);
+		var item = createItem(0,0);
 		expect(item.name).toEqual("test Item");
 		expect(item.sell_in).toEqual(0);
 		expect(item.quality).toEqual(0);
 	});
 
 	it("should lose quality and reduce sellIn time when a day passed", function(){
-		var item = new Item("test Item",1,1);
+		var item = createItem(1,1);
 		item.update_quality();
 		expect(item.sell_in).toEqual(0);
 		expect(item.quality).toEqual(0);
