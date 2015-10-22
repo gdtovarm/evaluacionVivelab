ffff681b2ce5b373dd14963b684de519617f4b21
German <gdtovarm@unal.edu.co>
2015-10-22 10:39:13 -0500

    added test to reduce the quality and sell in time when a day passes.

--- jasmine/spec/vivelabSpec.js
+++ jasmine/spec/vivelabSpec.js
@@ -5,4 +5,11 @@ describe("item", function(){
 		expect(item.sell_in).toEqual(0);
 		expect(item.quality).toEqual(0);
 	});
+
+	it("should lose quality and reduce sellIn time when a day passed", function(){
+		var item = new Item("test Item",1,1);
+		item.updateQuality();
+		expect(item.sell_in).toEqual(0);
+		expect(item.quality).toEqual(0);
+	});
 });
